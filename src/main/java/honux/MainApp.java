4b75edceaed6e9d88684d8c610d5f8c51224a06f
Hoyoung Jung <honux77@gmail.com>
2020-03-10 09:07:48 +0900

    Logback 적용
    SLF4j를 이용해서 logback을 적용하였다.

--- src/main/java/honux/MainApp.java
+++ src/main/java/honux/MainApp.java
@@ -1,16 +1,24 @@
 package honux;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
     public final static int DISTANCE = 100;
+    private static final Logger logger
+            = LoggerFactory.getLogger(MainApp.class);
 
     public static void main(String[] args) {
+        logger.info("======== {} Start =============", MainApp.class.getSimpleName());
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
         Turtle t = (Turtle) ctx.getBean("turtle");
-        t.go(DISTANCE);
+        int result = t.go(DISTANCE);
+        logger.debug("race result: {}", result);
+        logger.info("======== End ============");
+
     }
 }

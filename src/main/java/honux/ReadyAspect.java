19d5c47ee13c1988c08aee693e313930c54143e3
Hoyoung Jung <honux77@gmail.com>
2020-03-17 23:34:24 +0900

    Apply @Around Annotation
    Aspoect에 Around Annotation을 적용해 봄!

--- src/main/java/honux/ReadyAspect.java
+++ src/main/java/honux/ReadyAspect.java
@@ -1,15 +1,36 @@
 package honux;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 
 @Aspect
 public class ReadyAspect {
+
+    private Logger logger = LoggerFactory.getLogger(ReadyAspect.class);
+
     @Before("execution(* honux.Animal.go(..))")
-    public void readyAndGo(JoinPoint joinPoint) {
-        System.out.println("준비, 땅!");
+    public void start(JoinPoint joinPoint) {
+        System.out.println("경기를 시작합니다!");
+    }
+
+    @Around("execution(* honux.Animal.go(..))")
+    public Object readyAndGo(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object proceed = joinPoint.proceed();
+
+        stopWatch.stop();
+        logger.info(stopWatch.prettyPrint());
+
+        return proceed;
     }
 
     @After("execution(* honux.Animal.go(..))")
--- src/main/java/honux/SpringConfiguration.java
+++ src/main/java/honux/SpringConfiguration.java
@@ -1,6 +1,5 @@
 package honux;
 
-import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
